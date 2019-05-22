cc255298dc64199f7eff97327db6275745b05e04
iseungmin <iseungminial@gmail.com>
2019-05-22 13:08:56 +0200

    22mayo

---  TrabajoFinalM1/genera_slides.bat
+++
@@ -1,1 +0,0 @@
-pandoc -t revealjs --css=reveal.js/css/theme/black.css --standalone --variable theme=black --variable transition=slide --template=template-revealjs.html 0presentacion.md -o 0_presentacion.html
